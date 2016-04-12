531dde2865aa275d3b6790b69a9daf0efb9eb93c
Euan T <euantorano@users.noreply.github.com>
2016-04-12 13:09:14 +0100

    Added fallback for non-openbsd systems.

--- pledge.nim
+++ pledge.nim
@@ -2,14 +2,19 @@
 
 import os
 
-proc pledge_c(promises: cstring, paths: cstringArray): cint {.importc: "pledge".}
+when defined(openbsd):
+  proc pledge_c(promises: cstring, paths: cstringArray): cint {.importc: "pledge".}
 
-proc pledge*(promises: string): bool {.raises: [OSError].} =
-  ## Pledge to use only the defined functions, separated by a space.
-  let pledged = pledge_c(promises, nil)
+  proc pledge*(promises: string): bool {.raises: [OSError].} =
+    ## Pledge to use only the defined functions, separated by a space. Always returns true on non-OpenBSD systems.
+    let pledged = pledge_c(promises, nil)
 
-  if pledged != 0:
-    let errorCode = osLastError()
-    raiseOSError(errorCode)
+    if pledged != 0:
+      let errorCode = osLastError()
+      raiseOSError(errorCode)
 
-  return pledged == 0
+    return pledged == 0
+else:
+  proc pledge*(promises: string): bool =
+    ## Pledge to use only the defined functions, separated by a space. Always returns true on non-OpenBSD systems.
+    return true
