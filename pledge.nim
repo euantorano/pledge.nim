c0dce6dd4cf33daad2df5f36a06ff7927262cee5
Euan T <euantorano@users.noreply.github.com>
2016-04-12 12:09:46 +0100

    Update pledge.nim

--- pledge.nim
+++ pledge.nim
@@ -1,8 +1,9 @@
 # pledge(2) for Nim
 
-proc pledge_c(promises, paths: cstring): cint {.importc: "pledge".}
+proc pledge_c(promises: cstring, paths: cstringArray): cint {.importc: "pledge".}
 
 proc pledge*(promises: string): bool =
     ## Pledge to use only the defined functions, separated by a space.
+    # TODO: Paths
     let pledged = pledge_c(promises, nil)
     return pledged == 0
