1d90269a7e800f9dceca48509e53c38150aef818
Euan T <euantorano@users.noreply.github.com>
2016-04-12 12:51:08 +0100

    Throwing exception when pledging fails

--- pledge.nim
+++ pledge.nim
@@ -1,9 +1,15 @@
 # pledge(2) for Nim
 
+import os
+
 proc pledge_c(promises: cstring, paths: cstringArray): cint {.importc: "pledge".}
 
-proc pledge*(promises: string): bool =
-    ## Pledge to use only the defined functions, separated by a space.
-    # TODO: Paths
-    let pledged = pledge_c(promises, nil)
-    return pledged == 0
+proc pledge*(promises: string): bool {.raises: [OSError].} =
+  ## Pledge to use only the defined functions, separated by a space.
+  let pledged = pledge_c(promises, nil)
+
+  if pledged != 0:
+    let errorCode = osLastError()
+    raiseOSError(errorCode)
+
+  return pledged == 0
